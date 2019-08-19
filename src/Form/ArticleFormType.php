de8d2426466f635ac3567e0425fdb9ed7fbed28d
befabry <fabrybenjamin5@gmail.com>
2019-08-19 18:27:09 +0200

    Forms : 10 - Validation Constraints with @Assert

--- src/Form/ArticleFormType.php
+++ src/Form/ArticleFormType.php
@@ -3,22 +3,49 @@
 namespace App\Form;
 
 use App\Entity\Article;
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ArticleFormType extends AbstractType
 {
+    /**
+     * @var UserRepository
+     */
+    private $userRepository;
+
+    public function __construct(UserRepository $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
             ->add('title', TextType::class, [
                 'help' => 'Choose something catchy !'
             ])
-            ->add('content')
-            ->add('publishedAt', null, [
-                'widget' => 'single_text'
+            ->add('content', TextareaType::class)
+            ->add('publishedAt', DateTimeType::class, [
+                'widget' => 'single_text',
+                'html5' => true,
+                //@TODO delete data (=> default value), only to avoid installing a datepicker
+                'data' => new \DateTime(),
+            ])
+            ->add('author', EntityType::class, [
+                'class' => User::class,
+                'choice_label' => function(User $user){
+                    return sprintf('(%d) %s - %s', $user->getId(), $user->getEmail(), $user);
+                },
+                'placeholder' => 'Choose an author',
+                'choices' => $this->userRepository->findAllEmailAlphabetical(),
+                'invalid_message' => 'Symfony is too smart for your hacking !'
             ])
         ;
     }
